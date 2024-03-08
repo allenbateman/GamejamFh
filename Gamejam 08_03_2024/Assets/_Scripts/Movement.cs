d425001e4149582c21ea18fc31ba421600423593
Allen Bateman <57528826+allenbateman@users.noreply.github.com>
2024-03-08 20:58:14 +0100

    Player rotation

--- Gamejam 08_03_2024/Assets/_Scripts/Movement.cs
+++ Gamejam 08_03_2024/Assets/_Scripts/Movement.cs
@@ -1,22 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     Rigidbody rb;
+    Vector3 direction;
     public float speed;
     public float rotationSpeed;
-    // Start is called before the first frame update
+
+    Vector3 mouseWorldPoss;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector3 direction = Vector3.zero;
+        direction = Vector3.zero;
         if (Input.GetKey(KeyCode.W))
         {
             direction += new Vector3(0,0,1);
@@ -33,10 +35,60 @@ public class Movement : MonoBehaviour
         {
             direction += new Vector3(1, 0, 0);
         }
-        rb.velocity = direction * speed;
 
-        Quaternion newRot = Quaternion.identity;
 
-        rb.rotation = Quaternion.RotateTowards(rb.rotation, newRot, rotationSpeed);
+
+    }
+
+    Vector3 GetDirection()
+    {
+
+        Vector3 direction = Vector3.zero;
+        Vector3 mousePos = Input.mousePosition;
+
+        mousePos.z = Camera.main.nearClipPlane;
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos + new Vector3(0, 0, 50));
+        mouseWorldPos.y = 1;
+
+        direction = mouseWorldPos - transform.position;
+        direction.y = 0;
+
+        //
+        Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //create a plane at  heigh 1
+        Plane groundPlane = new Plane(Vector3.up, new Vector3(0,1,0));
+        float rayLength;
+
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            mouseWorldPoss = pointToLook;
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.cyan);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+
+        //
+
+
+        return direction;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = direction.normalized * speed;
+        Vector3 lookDir = GetDirection();
+
+        if (lookDir != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(lookDir);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(mouseWorldPoss, 1);
     }
 }

b14cb1a63ec782822050e248a8978fe46a527252
Allen Bateman <57528826+allenbateman@users.noreply.github.com>
2024-03-08 19:32:08 +0100

    Powerup vfx + interaction types

---
+++ Gamejam 08_03_2024/Assets/_Scripts/EarthPower.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthPower : Interactable
+{
+    public override void BaseInteract()
+    {
+        Debug.Log("earth");
+    }
+}
--- Gamejam 08_03_2024/Assets/_Scripts/Interactable.cs
+++ Gamejam 08_03_2024/Assets/_Scripts/Interactable.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    public virtual void Interact()
-    {
+     public virtual void Interact()
+     {
         BaseInteract();
-    }
+     }
 
-   public virtual void BaseInteract()
-   {
+    public virtual void BaseInteract()
+    {
         Debug.Log(gameObject.name + " interacting");
-   }
+    }
 }

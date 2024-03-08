7fe7ca13f77019c9b847975818baac59027a3ae8
maja.norgaard@gmail.com <maja>
2024-03-08 19:34:23 +0100

    movement

---
+++ Gamejam 08_03_2024/Assets/_Scripts/Movement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField]
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += new Vector3(0,0,1);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += new Vector3(1, 0, 0);
+        }
+        rb.velocity = direction * speed;
+    }
+}

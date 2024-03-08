5197d70d72031d7ba8c67d81f4384afed40532aa
Linus132 <162727599+Linus132@users.noreply.github.com>
2024-03-08 20:28:54 +0100

    add bullet script

--- Gamejam 08_03_2024/Assets/_Scripts/Bullet.cs
+++ Gamejam 08_03_2024/Assets/_Scripts/Bullet.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-
+    public float speed = 10.0f;
     public float damage = 1;
+
     // Start is called before the first frame update
     void Start()
     {

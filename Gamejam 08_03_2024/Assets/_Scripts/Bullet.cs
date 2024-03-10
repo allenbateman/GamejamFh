3712da0bc6be39a521bf5bffbd7708d41ed7c3d5
Linus132 <162727599+Linus132@users.noreply.github.com>
2024-03-10 10:36:13 +0100

    add state to bullet

--- Gamejam 08_03_2024/Assets/_Scripts/Bullet.cs
+++ Gamejam 08_03_2024/Assets/_Scripts/Bullet.cs
@@ -4,29 +4,40 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public enum BulletState {Water, Fire, Stone};
+
+
+    BulletState bulletState;
     public float speed = 10;
     public float damage = 1;
-    Rigidbody rb;  
-    float destroytime = 2;
-    public PowerTypes powerType = PowerTypes.None;
-    public float damageMultiplier = 1.5f;
+    public float destroyTime = 3;
+    Rigidbody rb;
+    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.AddForce(transform.forward * speed, ForceMode.Impulse);
-        GetComponent<Rigidbody>().AddForce(transform.forward * speed, ForceMode.Impulse);
+        Destroy(gameObject, destroyTime);
 
     }
+
+    // Update is called once per frame
     void Update()
     {
-        destroytime -= Time.deltaTime;
-        if(destroytime < 0) 
-            Destroy(gameObject);
+        //stransform.Translate(transform.forward * speed * Time.deltaTime);
+        
     }
 
-    private void OnCollisionEnter(Collision collision)
+    public void destroy()
     {
-       // Destroy(gameObject);
+        Destroy(gameObject, 0.5f);
+        
     }
 
+    public void setBulletState(BulletState bulletState)
+    {
+        this.bulletState = bulletState;
+    }
 }
+
+
